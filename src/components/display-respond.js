5b1fb390533f46d6546dc3039b5347660b856bbb
Gyorgy Gabor <gyorgyg@gmail.com>
2018-05-25 22:08:48 +0200

    feat(display respond): render the respond of the server 10min

---
+++ src/components/display-respond.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const displayRespond = (() => {
+
+  const displayRespond = (result) => {
+    console.log(result);
+    const modalFooter = document.querySelector('.modal-footer');
+    const response = document.createElement('p');
+
+    modalFooter.innerHTML = '';
+    modalFooter.setAttribute('style', 'background: white;')
+    response.textContent = `The response is: ${result.status}`;
+    modalFooter.appendChild(response);
+  }
+
+  return {
+    displayRespond: displayRespond
+  }
+})();
