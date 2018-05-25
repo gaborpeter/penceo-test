111c8b2b8fe28533708a350d58ead1fcbd120991
Gyorgy Gabor <gyorgyg@gmail.com>
2018-05-25 21:36:09 +0200

    feat(close button): close modal window 5min

---
+++ src/components/close.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const close = (() => {
+
+  const close = () => {
+    const closeButton = document.querySelector('#close');
+    const modalWindow = document.querySelector('.login-modal')
+
+    closeButton.addEventListener('click', event => {
+      modalWindow.style.visibility = 'hidden';
+    })
+  }
+
+  return {
+    close: close
+  }
+})();
--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ const index = (() => {
   start.start();
   forgetPassword.getNewPassword();
   login.login();
+  close.close();
 })();
