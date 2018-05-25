bb81754a0f24c4c5bab7f0466cedd85ab8ce85f6
Gyorgy Gabor <gyorgyg@gmail.com>
2018-05-25 17:17:26 +0200

    feat(forgotten passwrd button): change view

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname +  '/public/index.html')
 });
 
+app.get('/password', function(req, res) {
+  console.log('sdsd');
+  res.sendFile(__dirname +  '/public/password.html')
+});
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
 })
